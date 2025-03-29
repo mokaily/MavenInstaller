3a6cc0e3e25ac520337a8bca3579852adcc0860a
Mohamed Okaily <midostaar30@gmail.com>
2025-03-29 03:17:36 +0100

    Rename Main file

--- src/main/java/com/example/maveninstaller/Main.java
+++ src/main/java/com/example/maveninstaller/Main.java
@@ -2,7 +2,7 @@ package com.example.maveninstaller;
 
 import javax.swing.*;
 
-public class GitHubCloneApp {
+public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             SystemCheck.checkAndDisplay();
